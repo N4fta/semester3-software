8892963f37328677ee929636c8009ff861abd859
Neo <540432@fhict.nl>
2024-09-18 14:06:15 +0200

    added starter project week 2 rest api

---
+++ week3/js/main.js
@@ -0,0 +1,17 @@
+// An array of objects, which each represent a user account
+const userAccounts = [
+    {
+        username: 'john@example.com',
+        password: 'john'
+    },
+    {
+        username: 'sarah@example.com',
+        password: 'sarah'
+    },
+    {
+        username: 'hector@example.com',
+        password: 'hector'
+    }
+];
+
+console.log('The user accounts have been initialised!');
