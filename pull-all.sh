1721c60af9f68d87cebbd18871a45a0fd2c018d0
N4fta <nunocdias@outlook.com>
2024-09-25 19:48:42 +0200

    Upgraded the pull/push script

--- pull-all.sh
+++
@@ -1,1 +0,0 @@
-find . -mindepth 1 -maxdepth 3 -type d -name .git -exec git -C . pull \;
---
+++ sync-all.sh
@@ -0,0 +1,20 @@
+# Save the current directory so it can be returned to after the loop is done.
+home_dir=$(pwd)
+
+# Find all .git folders in the current directory and its subdirectories
+find . -maxdepth 3 -name ".git" | while read git_dir; do
+  # Change to the .git folder and go back up one level since git commands cannot be run from a .git folder
+  cd "$git_dir/.." || continue
+
+  echo "Committing in $(pwd)"
+
+  git pull -q                                     # Pull the latest changes quietly
+  git add -A                                      # Add all changes to the staging area
+  git commit --short -m "Auto-committing changes" # Commit with a short message
+  git push -q                                     # Push the changes quietly
+
+  echo ""
+
+  # Return to the home directory
+  cd "$home_dir"
+done
