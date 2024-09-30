5ae176ed071534800de18ccfc8a625090301cc6c
Neo <540432@fhict.nl>
2024-09-30 19:46:38 +0200

    week5 ex

---
+++ week5/todolist-starterproject/src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+
+function NavBar() {
+  const links = [
+    {
+      id: 1,
+      path: "/",
+      text: "Todo List",
+    },
+    {
+      id: 2,
+      path: "/user",
+      text: "User",
+    },
+  ];
+
+  return (
+    <nav className={styles.navBar}>
+      <ul>
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              {link.path}
+              {link.text}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
